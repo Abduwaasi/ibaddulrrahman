22508eab833c57a095eacfbebde05d03398d5095
Adebayo Abdulwaasi <wasiuadekunle73@gmail.com>
2021-12-23 09:40:00 +0100

    validation code added to the website

--- src/Scripts/form.js
+++ src/Scripts/form.js
@@ -5,6 +5,7 @@ console.log(steps)
 const nextBtn = document.querySelectorAll("form .next-btn");
 const prevBtn = document.querySelectorAll("form .previous-btn");
 const form = document.querySelector("form");
+const nextPersonBtn = document.querySelector("form .next-person")
 
 nextBtn.forEach((button) => {
   button.addEventListener("click", () => {
@@ -17,17 +18,37 @@ prevBtn.forEach((button) => {
   });
 });
 
+nextPersonBtn.addEventListener("click", ()=>{
+  changeStep("nextPerson");
+})
+
 
 function changeStep(btn) {
   let index = 0;
   const active = document.querySelector(".active");
   index = steps.indexOf(active);
+  console.log(index)
   steps[index].classList.remove("active");
   if (btn === "next") {
     index++;
   } else if (btn === "prev") {
     index--;
   }
+  // else if (btn==="nextPerson"){
+  //   for (let i = 0; i < 3; i++) {
+  //     index=3;
+  //     active.reset()
+     
+      
+  //   }
+  //   // form.querySelectorAll("input").forEach((input) => {
+  //   //       const inputs = []
+  //   //       const { name, value } = input;
+  //   //       inputs.push({ name, value });
+  //   //       console.log(inputs);
+  //   //       form.reset();
+  //   //     });
+  // }
   steps[index].classList.add("active");
 }
  
